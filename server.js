bb6a92148e2fe64886848d4eef4ca218de6de0bb
science-math-guy <adumont@mailbox.org>
2021-05-06 01:10:13 +0200

    Changes

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-const express = require('express');
-const chalk = require('chalk');
-const ejs = require('ejs');
+const express = require("express");
+const chalk = require("chalk");
+const ejs = require("ejs");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+// app.set("trust proxy", true);
+// app.use((req, res, next) => {
+//   if (!req.secure) return res.redirect("https://" + req.get("host") + req.url);
+//   next();
+// });
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/map', (req, res) => {
-    res.render('map');
+app.get("/map", (req, res) => {
+  res.render("map");
+});
+
+app.get("/images", (req, res) => {
+  res.render("images");
 });
 
 app.listen(PORT, () => {
-    console.log(chalk.bold.blue(`Server listening on port ${PORT}...`));
+  console.log(chalk.bold.blue(`Server listening on port ${PORT}...`));
 });
