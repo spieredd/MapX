fb7e25daf7e6dd31eb4ad929662722392935e5c4
science-math-guy <adumont@mailbox.org>
2021-06-01 02:06:41 +0200

    Working review

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const chalk = require("chalk");
 const ejs = require("ejs");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use('/web/app', express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.set("view engine", "ejs");
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/web/app", express.static("public"));
 
 app.get("/", (req, res) => {
   res.redirect("/web/app/home");
@@ -40,10 +42,34 @@ app.get("/web/app/resources/personal-case-study", (req, res) => {
   res.render("pages/personal");
 });
 
-app.post('/web/app/contact/review', (req, res) => {
-  console.log(req);
-  res.render('pages/contact.ejs', { ok: true });
-})
+app.post("/web/app/contact", (req, res) => {
+  console.log(req.body);
+  res.render("pages/contact.ejs", { ok: true });
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  });
+
+  let professor = process.env.MAIL2
+
+  let mailOptions = {
+    from: process.env.MAIL,
+    to: process.env.MAIL,professor,
+    subject: `WW1 Map Project - Automatic Review Email`,
+    html: `<div style="border-radius:5px;padding:20px;background-color:#4a1c40;color:#fff"><p style="color:#962d2d">This is an automatic email sent when a person leaves a review on our WW1 Class Project Website: </p> <h3>person (can be anonymous):${req.body.username}</h3><h2>Content of the message</h2><p>${req.body.review}</p><h3>Cordially,</h3><h3>Adrien DUMONT.</h3></div>`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(chalk.bold.blue(`Server listening on port ${PORT}...`));
